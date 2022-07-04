19cc79e9d42fa1f5d197a54208166332fcf586d5
c4rls <ssantos.carloseduardo@gmail.com>
2022-07-04 09:31:25 -0300

    :sparkles: Resoluções das questões sql

---
+++ resolucao-2606.sql
@@ -0,0 +1,4 @@
+SELECT p.id, p.name FROM
+    products AS p
+    INNER JOIN categories AS c
+    ON p.id_categories = c.id WHERE LOWER(c.name) LIKE 'super%';
---
+++ resolucao-2622.sql
@@ -0,0 +1,2 @@
+SELECT customers.name
+FROM legal_person INNER JOIN customers ON legal_person.id_customers = customers.id;
